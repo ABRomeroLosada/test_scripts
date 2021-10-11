aa376d83b89d7c6c18e10a89bd8bf3d4c15ffe29
Ana Belén Romero-Losada <arlosada@us.es>
2021-10-11 11:00:03 +0200

    processed data added

--- Testing_waves_parameters.R
+++ Testing_waves_parameters.R
@@ -1,1 +1,15 @@
 library(circacompare)
+
+gene.expression <- read.table(file="../gene_expression.tsv",header = T,sep = "\t")
+gene.expression.SD <- gene.expression[,31:48]
+
+gene.expression <- read.table(file="../gene_expression.tsv",header = T,sep = "\t")
+gene.expression.LD <- gene.expression[,1:18]
+
+swath.normalized.data.SD <- read.table(file = "../sd_swath_processed_data.tsv",header=T,sep="\t")
+rownames(swath.normalized.data.SD)<- swath.normalized.data.SD$X
+swath.normalized.data.SD$X <- NULL
+
+swath.normalized.data.LD <- read.table(file = "../swath_processed_data.tsv",header=T,sep="\t")
+swath.normalized.data.LD[,"zt20_2"] <- swath.normalized.data.LD[,"zt16_2"]
+swath.normalized.data.LD[,"zt16_2"] <- swath.normalized.data.LD[,"zt20_2"]
