d8b9ed15080e1f217cb4e12ac2de6f22b13637a8
Ana Belén Romero-Losada <arlosada@us.es>
2021-10-13 11:35:56 +0200

    testing waves

--- Testing_waves_parameters.R
+++ Testing_waves_parameters.R
@@ -11,8 +11,9 @@ gene.expression.LD <- gene.expression[,1:42]
 gene.expression.LD.DD <- gene.expression[,31:42]
 gene.expression.LD.LL <- gene.expression[,19:30]
 
-gene <- "ostta01g00880"
 
+gene <- "ostta01g00880"
+gene <- "ostta03g03470"
 time.points <- seq(from=0,by=4,length.out = 12)
 circacompare.LL.DD <- matrix(ncol=1,nrow=15)
 
@@ -28,21 +29,21 @@ result.i<- circacompare(x = circacomp.data,
                         col_group = "group", 
                         col_outcome = "measure",
                         alpha_threshold = 1)
-png(file="circacompareDDLLostta01g00880.png")
+png(file="circacompareDDLLostta03g03470.png")
 result.i$plot
 dev.off()
 
 #probando con radianes
 t <- seq(from=0, to=10, by=0.5)
-wave.DD= 5.487292e-01 + 3.977647e-01 * cos( 4.190e11 * (t-8.005159))
-wave.LL= 7.339324e-01 + 1.429324e-01 * cos( 4.190e11 * (t-2.345114e+01))
+wave.DD= 0.4766370219 + 0.4127642249 * cos( 4.190e11 * (t-23.8471404982))
+wave.LL= 0.7611205567 + 0.1724573355 * cos( 4.190e11 * (t-23.7558093743))
 
-##probando con h xq se supone que cicacompare trata con h
-t<-seq(from=0, to=20, by=0.83)
-wave.DD= 5.487292e-01 + 3.977647e-01 * cos( 24 * (t-8.005159))
-wave.LL= 7.339324e-01 + 1.429324e-01 * cos( 24 * (t-2.345114e+01))
+# ##probando con h xq se supone que cicacompare trata con h
+# t<-seq(from=0, to=20, by=0.83)
+# wave.DD= 5.487292e-01 + 3.977647e-01 * cos( 24 * (t-8.005159))
+# wave.LL= 7.339324e-01 + 1.429324e-01 * cos( 24 * (t-2.345114e+01))
 
-wave.SD <- wave.DD + wave.LL
+wave.SD <- (wave.DD) + wave.LL
 plot(x= t, 
      y=wave.DD/max(wave.DD), 
      type="o",col="red",
@@ -56,3 +57,7 @@ lines(x=t,
       y=wave.SD/max(wave.SD),
       type="o", col="red",
       lwd=3, lty=1)
+legend("bottomleft", 
+       legend = c("DD", "LL", "DD+LL"), 
+       lwd=2, col = c("red", "orange", "red"), 
+       lty=c(3,3,1))
