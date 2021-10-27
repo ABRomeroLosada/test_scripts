e5886949291a342e17b1997c78f70a907b2fe0a6
Ana Belén Romero-Losada <arlosada@us.es>
2021-10-27 14:27:29 +0200

    adding functions for fourier

--- Testing_waves_parameters.R
+++ Testing_waves_parameters.R
@@ -6,6 +6,7 @@ gene.expression$X <- NULL
 gene.expression.SD <- gene.expression[,43:84]
 gene.expression.SD.DD <- gene.expression[,73:84]
 gene.expression.SD.LL <- gene.expression[,61:72]
+names(gene.expression.SD.LL)
 
 gene.expression.LD <- gene.expression[,1:42]
 gene.expression.LD.DD <- gene.expression[,31:42]
@@ -118,3 +119,110 @@ colnames(circacompare.DD.DD) <- result.i[[2]][,1]
 hist(circacompare.DD.DD[,13])
 mean(circacompare.DD.DD[,13])
 
+######Datos de marcos
+#Function for extracting the strenght and frequency of the waves that compose the resulting one
+#as calculated by GeneCycle
+#circ.data <- as.numeric(fousdll)
+extract.individual.waves <- function(circ.data) #n.samples must be even
+{
+   perio <- periodogram(circ.data)
+   waves <- matrix(nrow = length(circ.data)/2, ncol = 2)
+   for (i in 1:length(circ.data)/2)
+   {
+      waves[i,1] <- perio$freq[i]*length(circ.data)
+      waves[i,2] <- perio$spec[i]
+   }
+   return(waves)
+}
+
+# Function for extracting the trajectory from fft output
+get.trajectory <- function(X.k,ts,acq.freq) {
+   
+   N   <- length(ts)
+   i   <- complex(real = 0, imaginary = 1)
+   x.n <- rep(0,N)           # create vector to keep the trajectory
+   ks  <- 0:(length(X.k)-1)
+   
+   for(n in 0:(N-1)) {       # compute each time point x_n based on freqs X.k
+      x.n[n+1] <- sum(X.k * exp(i*2*pi*ks*n/N)) / N
+   }
+   
+   x.n * acq.freq 
+}
+
+#Function to draw each of the simple waves that make up the complex waveform
+plot.harmonic <- function(Xk, i, ts, acq.freq, color="red") {
+   Xk.h <- rep(0,length(Xk))
+   Xk.h[i+1] <- Xk[i+1] # i-th harmonic
+   harmonic.trajectory <- get.trajectory(Xk.h, ts, acq.freq=acq.freq)
+   points(ts, harmonic.trajectory, type="l", col=color)
+}
+
+# Function for drawing the periodogram of a complex wave to determine the contribution
+#of each wave to the complex pattern
+draw.periodogram <- function(circ.data, harmonics=6)
+{
+   period.data <- periodogram(circ.data)
+   plot(period.data$freq[1:harmonics]*length(circ.data), 
+        period.data$spec[1:harmonics]/sum(period.data$spec), 
+        xlab="Harmonics (Hz)", ylab="Amplitute Density", type="h")
+}
+
+###Más funciones
+wave.decomposition.scaled <- function(data.fou, n.waves,time=48, acq.freq=0.25, ts=seq(0,44,4))
+{
+   waves <- extract.individual.waves(data.fou)
+   colnames(waves) <- c("freq", "strength")
+   data.fft <- fft(data.fou)
+   names(data.fft) <- NULL
+   wave.order <- order(waves[,2],decreasing = T)
+   x.n <- get.trajectory(data.fft,ts,acq.freq)
+   x.n <- x.n/max(Mod(x.n))
+   amp <- max(Mod(x.n)) - min(Mod(x.n))
+   plot(ts,x.n,type="l",lwd=2, ylim=c(0,1))
+   wave.colors <- c("red", "blue", "green", "purple", "orange", "gray")
+   fft.vector <- rep(0,length(data.fft))
+   for (j in n.waves)
+   {
+      #j<-1
+      fft.vector[wave.order[j]+1] <- data.fft[wave.order[j]+1]
+      fft.vector[length(ts)+2-(wave.order[j]+1)] <- data.fft[length(ts)+2-(wave.order[j]+1)]
+   }
+   fft.vector[1] <- data.fft[1]
+   harmonic.trajectory <- get.trajectory(fft.vector, ts, acq.freq=acq.freq)
+   harmonic.trajectory <- harmonic.trajectory/max(Mod(harmonic.trajectory))
+   points(ts, harmonic.trajectory, type="l",lty=2, col="darkred", lwd=3)
+   
+   return(list(wave.params=waves, wave.points=data.fft,freqs.order=wave.order,comb.wave=harmonic.trajectory))
+}
+photo.and.skoto <- function(gene.name, genes.expression, sd.ll, sd.cycle)
+{
+   fou <- genes.expression[gene.name,]
+   fousdll =fou[sd.ll]
+   wave.order <- wave.decomposition.scaled(as.numeric(fousdll), n.waves = 1:3, time=24, ts=seq(0,20,4))$freqs.order
+   order.pos <- which(wave.order == 1) #Extracts the wave with an associated period of 24h
+   dec.wave.ll <- wave.decomposition.scaled(as.numeric(fousdll), n.waves = order.pos, time=24, ts=seq(0,20,4))$comb.wave
+   
+   foutot <- fou[sd.cycle]
+   dec.wave.ll.2 <- wave.decomposition.scaled(as.numeric(foutot), n.waves = 1:6, time=48, ts=seq(0,44,4))$comb.wave
+   dec.wave.res.cycle <- dec.wave.ll.2 - dec.wave.ll
+   ext.dec.wave.cycle.order <- wave.decomposition.scaled(as.numeric(dec.wave.res.cycle), n.waves = 1:6)$freqs.order
+   ord.pos <- which(ext.dec.wave.cycle.order == 2) #Extracts the wave with an associated period of 24h
+   ext.dec.wave.cycle <- wave.decomposition.scaled(as.numeric(dec.wave.res.cycle), n.waves = ord.pos)$comb.wave
+   ext.dec.wave.cycle <- ext.dec.wave.cycle + 1 #Makes all points positive
+   ext.dec.wave.cycle <- ext.dec.wave.cycle/max(Mod(ext.dec.wave.cycle))
+   dec.wave.ll <- dec.wave.ll - min(Mod(dec.wave.ll))
+   dec.wave.ll <- dec.wave.ll/max(Mod(dec.wave.ll))
+   dec.wave.ll.2 <- dec.wave.ll.2 - min(Mod(dec.wave.ll.2))
+   dec.wave.ll.2 <- dec.wave.ll.2/max(Mod(dec.wave.ll.2))
+   plot(seq(0,44,4), ext.dec.wave.cycle, type="l",lty=2, col="darkgrey", lwd=3, ylim=c(-0.2,1),
+        main=gene.name, ylab = "Normalized amplitude", xlab="Time")
+   points(seq(0,44,4), rep(dec.wave.ll,2), type="l",lty=2, col="darkorange", lwd=3)
+   points(seq(0,44,4), dec.wave.ll.2, type="l", col="black", lwd=3) 
+   legend("bottomright", legend = c("photoperiod", "skotoperiod"), fill = c("darkorange","darkgrey"))
+   return(list(norm.wave = dec.wave.ll.2, norm.photo = dec.wave.ll, skoto.norm = ext.dec.wave.cycle))
+}
+
+photo.and.skoto(gene.name=gene, genes.expression = gene.expression,
+                sd.ll= names(gene.expression.SD.LL)[1:6],
+                sd.cycle = names(gene.expression.SD)[1:12])
