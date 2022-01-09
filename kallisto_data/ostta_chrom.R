d9001c16253d127448732d94f90a398097506dbb
Ana Belén Romero-Losada <arlosada@us.es>
2022-01-09 14:23:33 +0100

    new directory created

---
+++ kallisto_data/ostta_chrom.R
@@ -0,0 +1,12 @@
+## Encontrar secuencias de transcritos y construir el indice
+## kallisto index -i ostreococcus_tauri.idx ostreococcus_tauri_transcripts.fasta
+
+## allisto quant -i ostreococcus_tauri.idx -b 100 -o kallisto_out --genomebam --gtf ostreococcus_tauri.gtf --chromosomes chrom_ostreococcus_tauri.txt --single -l 200 -s 10 sd_zt20_7.fq.gz
+
+library(seqinr)
+ostta.genome.data <- read.fasta(file = "ostreococcus_tauri.fa.gz",seqtype = "DNA")
+ostta.genome.seqs <- getSequence(ostta.genome.data)
+
+
+chrom.data <- data.frame(getName(ostta.genome.data),sapply(X = ostta.genome.seqs,FUN = length))
+write.table(x = chrom.data,file = "chrom_ostreococcus_tauri.txt",quote = F,row.names = F,col.names = F, sep = "\t")
